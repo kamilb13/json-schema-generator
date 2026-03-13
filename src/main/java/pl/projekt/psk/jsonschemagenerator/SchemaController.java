0de25b96a0306241a3bffd0e3103e6e748c45f91
kamilb13 <kamil.brzoza13@gmail.com>
2026-03-13 19:53:19 +0100

    add readTree

--- src/main/java/pl/projekt/psk/jsonschemagenerator/SchemaController.java
+++ src/main/java/pl/projekt/psk/jsonschemagenerator/SchemaController.java
@@ -5,9 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 
 @Controller
 public class SchemaController {
+
+    private final ObjectMapper objectMapper;
+
+    public SchemaController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     @GetMapping("/")
     public String index() {
@@ -16,6 +24,7 @@ public class SchemaController {
 
     @PostMapping("/generate")
     public String generate(@RequestParam String jsonInput, Model model) {
+        JsonNode jsonNode = objectMapper.readTree(jsonInput);
         String hardcodedSchema = "{\n" +
                 "  \"location\": \"object\",\n" +
                 "  \"adress\": {\n" +
