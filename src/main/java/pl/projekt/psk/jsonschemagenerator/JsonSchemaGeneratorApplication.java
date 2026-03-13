a2f0f14df68ad855ec769fde11b311243ac401e1
kamilb13 <kamil.brzoza13@gmail.com>
2026-03-13 19:11:23 +0100

    init commit

---
+++ src/main/java/pl/projekt/psk/jsonschemagenerator/JsonSchemaGeneratorApplication.java
@@ -0,0 +1,13 @@
+package pl.projekt.psk.jsonschemagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonSchemaGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonSchemaGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/projekt/psk/jsonschemagenerator/JsonSchemaGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.projekt.psk.jsonschemagenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonSchemaGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
