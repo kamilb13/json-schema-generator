1064dc84b0b7076f7187758ed0bf2be323aee2ca
kamilb13 <kamil.brzoza13@gmail.com>
2026-03-13 19:47:29 +0100

    Add schema generation endpoint and improve UI

--- src/main/java/pl/projekt/psk/jsonschemagenerator/SchemaController.java
+++ src/main/java/pl/projekt/psk/jsonschemagenerator/SchemaController.java
@@ -1,13 +1,31 @@
 package pl.projekt.psk.jsonschemagenerator;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SchemaController {
 
     @GetMapping("/")
     public String index() {
+        return "index";
+    }
+
+    @PostMapping("/generate")
+    public String generate(@RequestParam String jsonInput, Model model) {
+        String hardcodedSchema = "{\n" +
+                "  \"location\": \"object\",\n" +
+                "  \"adress\": {\n" +
+                "    \"przykladzik\": {\n" +
+                "      \"type\": \"string\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        model.addAttribute("schemaOutput", hardcodedSchema);
+        model.addAttribute("jsonInput", jsonInput);
         return "index";
     }
 }
