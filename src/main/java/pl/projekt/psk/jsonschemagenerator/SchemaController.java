ee94b7cb563555d7390b3df0aa8badffc52201f4
kamilb13 <kamil.brzoza13@gmail.com>
2026-03-19 21:22:36 +0100

    Add isValidJson method, edit index.html error handler, correctly sending jsonInput to globalhandler using HttpServletRequest

--- src/main/java/pl/projekt/psk/jsonschemagenerator/SchemaController.java
+++ src/main/java/pl/projekt/psk/jsonschemagenerator/SchemaController.java
@@ -1,7 +1,8 @@
 package pl.projekt.psk.jsonschemagenerator;
 
-import org.springframework.ui.Model;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +24,8 @@ public class SchemaController {
     }
 
     @PostMapping("/generate")
-    public String generate(@RequestParam String jsonInput, Model model) {
+    public String generate(@RequestParam String jsonInput, Model model, HttpServletRequest request) {
+        request.setAttribute("jsonInput", jsonInput);
         if (jsonInput == null || jsonInput.isEmpty()) {
             throw new IllegalArgumentException("Json input cannot be null or empty");
         }
@@ -37,6 +39,18 @@ public class SchemaController {
                 "  }\n" +
                 "}";
         model.addAttribute("schemaOutput", hardcodedSchema);
+        model.addAttribute("jsonInput", jsonInput);
+        return "index";
+    }
+
+    @PostMapping("/checkJson")
+    public String isJsonValid(@RequestParam String jsonInput, Model model, HttpServletRequest request) {
+        request.setAttribute("jsonInput", jsonInput);
+        if (jsonInput == null || jsonInput.isEmpty()) {
+            throw new IllegalArgumentException("Json input cannot be null or empty");
+        }
+        objectMapper.readTree(jsonInput);
+        model.addAttribute("isValid", true);
         model.addAttribute("jsonInput", jsonInput);
         return "index";
     }
