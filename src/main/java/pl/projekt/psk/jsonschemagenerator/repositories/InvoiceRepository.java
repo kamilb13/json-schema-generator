91d8e998a32013b044b5b919ad7619a7b24f55fd
kamilb13 <kamil.brzoza13@gmail.com>
2026-04-09 20:06:29 +0200

    code fixes, fixes lombok and mongodb

--- src/main/java/pl/projekt/psk/jsonschemagenerator/repositories/InvoiceRepository.java
+++ src/main/java/pl/projekt/psk/jsonschemagenerator/repositories/InvoiceRepository.java
@@ -1,7 +1,9 @@
 package pl.projekt.psk.jsonschemagenerator.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import pl.projekt.psk.jsonschemagenerator.models.Invoice;
 
+@Repository
 public interface InvoiceRepository extends MongoRepository<Invoice, String> {
 }
--- src/main/java/pl/projekt/psk/jsonschemagenerator/repositories/JsonSchemaRepository.java
+++ src/main/java/pl/projekt/psk/jsonschemagenerator/repositories/JsonSchemaRepository.java
@@ -1,7 +1,9 @@
 package pl.projekt.psk.jsonschemagenerator.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import pl.projekt.psk.jsonschemagenerator.models.JsonSchema;
 
+@Repository
 public interface JsonSchemaRepository extends MongoRepository<JsonSchema, String> {
 }
