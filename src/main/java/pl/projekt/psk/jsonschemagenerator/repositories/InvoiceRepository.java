47be086bcf4e81c3b5440a448fde6c6d4d142d81
Mikołaj Pacierz <mikolajpacierz18@gmail.com>
2026-05-28 22:11:50 +0200

    ci: added Github Actions workflow for tests

--- src/main/java/pl/projekt/psk/jsonschemagenerator/repositories/InvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.projekt.psk.jsonschemagenerator.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import pl.projekt.psk.jsonschemagenerator.models.Invoice;
-
-@Repository
-public interface InvoiceRepository extends MongoRepository<Invoice, String> {
-}
